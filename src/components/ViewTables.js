1695bc7184c7dcc12ef08375abac9d193657cf7b
Franciscoamaral0 <franciscogurgel@me.com>
2021-08-11 05:18:59 +0100

    table structure almost ready

--- src/components/ViewTables.js
+++ src/components/ViewTables.js
@@ -1,64 +1,69 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import UnderHeaderImg from './UnderHeaderImg';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import { Table } from 'react-bootstrap';
+import axios from 'axios';
+import './tablesStyled.css';
 
 
 class Viewtables extends Component {
-  state = {  }
+  state = {  
+    time :[]
+    
+  }
+
+  componentDidMount(){
+    axios.get('https://api.api-futebol.com.br/v1/campeonatos/14/tabela',{headers:{'Authorization': 'Bearer test_cd742736980799740abf5ccaa74d2e' }}).then(result =>{
+      const {data} = result
+      
+      this.setState({
+        time: data
+        
+      })
+    })
+  }
+
+  renderTableData= () =>{
+    return this.state.time.map((element, index) =>{
+      return(
+        <tr key={element.time.time_id}>
+        <td>{element.posicao}</td>
+        <td><img width='23px' src={element.time.escudo}/></td>
+        <td>{element.time.nome_popular}</td>
+        <td>{element.aproveitamento}</td>
+        </tr>
+      )
+    })
+  }
+
+  renderTableHeader = () => {
+    const headerNames = ['#', '', 'CLUBE', 'PONTOS' ]
+    return headerNames.map((element,index) => {
+      return <th key={element.index}>{element}</th>
+    })
+  }
+
+
   
   render() { 
     return (  
       <>
+      
       <Header/>
       <UnderHeaderImg children='Brasileirão'/> 
       <div style={{backgroundImage: `url("https://i.pinimg.com/originals/81/b2/07/81b20736e3201de30766c8b5ba69673b.jpg")`}} className='  p-5'>
       <div className='shadow-sm p-5 bg-body rounded d-flex  align-self-baseline '>
       <div className=' w-100 d-flex justify-content-evenly'>
-        <div>
-          <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th></th>
-                  <th>Clube</th>
-                  <th>Pontos</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{this.props.position}dasdasdadasdadasda</td>
-                  <td>{this.props.escudo}dasdasdasdas</td>
-                  <td>{this.props.clube}dasdasdas</td>
-                  <td>{this.props.pontos}dasdasdas</td>
-                </tr>
-                
-              </tbody>
-            </Table>
-          </div>
-
+        
           <div>
-          <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Posição</th>
-                  <th>Clube</th>
-                  <th>Pontos</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{this.props.position}1</td>
-                  <td>{this.props.escudo}ATA</td>
-                  <td>{this.props.clube}<img width='30px' src='https://apifutebol.s3.sa-east-1.amazonaws.com/escudos/5f999d8090849.svg'/></td>
-                  <td>{this.props.gols}Gabigol</td>
-                </tr>
-                
-              </tbody>
-            </Table>
+            <h2 id='title'>Classificação</h2>
+              <table id='clubs'>
+                <tbody>
+                <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+                </tbody>
+              </table>
           </div>
+                
 
         
       </div>
@@ -71,3 +76,6 @@ class Viewtables extends Component {
 }
  
 export default Viewtables ;
+
+
+ 
