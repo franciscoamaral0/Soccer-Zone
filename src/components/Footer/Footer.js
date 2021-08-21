4ef4be9b5d298858b0856c71438bb32a0474b890
Franciscoamaral0 <franciscogurgel@me.com>
2021-08-21 14:00:30 +0100

    repaired tag <a>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,7 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub'
+import {Link} from "react-router-dom";
 
 class Footer extends Component {
 
@@ -12,10 +13,10 @@ class Footer extends Component {
         <div className='mt-3 mb-2 text-center '>
           <h3 className = 'm-2 text-black'>Follow Arthur</h3>
           <div className = 'mt-4 mb-3 d-flex justify-content-evenly'>
-            <a style = {{color: 'inherit'}} className='text-decoration-none black' href="https://www.instagram.com/arthur.melo95/" target='_blank'><InstagramIcon color = 'disable' fontSize = 'large'/></a>
-            <a style = {{color: 'inherit'}} href="https://m.facebook.com/arthur.melo.12" target='_blank'><FacebookIcon color = 'disable' fontSize = 'large'/></a>
-            <a style = {{color: 'inherit'}} href="https://www.linkedin.com/in/arthur-melo-717526146/" target='_blank'><LinkedInIcon color = 'disable' fontSize = 'large'/></a>
-            <a style = {{color: 'inherit'}} href="https://github.com/arthurmelo10" target='_blank'><GitHubIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} className='text-decoration-none black' rel="noopener noreferrer" href="https://www.instagram.com/arthur.melo95/" target='_blank'><InstagramIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://m.facebook.com/arthur.melo.12" rel="noopener noreferrer" target='_blank'><FacebookIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://www.linkedin.com/in/arthur-melo-717526146/" rel="noopener noreferrer" target='_blank'><LinkedInIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://github.com/arthurmelo10" rel="noopener noreferrer" target='_blank'><GitHubIcon color = 'disable' fontSize = 'large'/></a>
           </div>
         </div>
         <div className = 'mt-5 text-center'>
@@ -24,10 +25,10 @@ class Footer extends Component {
         <div className=' text-decoration-none mt-3 mb-2 text-center'>
           <h3 className = 'm-2 text-black'>Follow Francisco</h3>
           <div className = 'mt-4 mb-3 d-flex justify-content-evenly'>
-            <a style = {{color: 'inherit'}} href="https://www.instagram.com/franciscogurgel0/" target='_blank'><InstagramIcon color = 'disable' fontSize = 'large'/></a>
-            <a style = {{color: 'inherit'}} href="https://www.facebook.com/franciscogurgel777" target='_blank'><FacebookIcon color = 'disable' fontSize = 'large'/></a>
-            <a style = {{color: 'inherit'}} href="https://www.linkedin.com/in/francisco-amaral-3b2490163/" target='_blank'><LinkedInIcon color = 'disable' fontSize = 'large'/></a>
-            <a style = {{color: 'inherit'}} href="https://github.com/franciscoamaral0" target='_blank'><GitHubIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://www.instagram.com/franciscogurgel0/" rel="noopener noreferrer" target='_blank' ><InstagramIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://www.facebook.com/franciscogurgel777" rel="noopener noreferrer" target='_blank' ><FacebookIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://www.linkedin.com/in/francisco-amaral-3b2490163/" rel="noopener noreferrer" target='_blank' ><LinkedInIcon color = 'disable' fontSize = 'large'/></a>
+            <a style = {{color: 'inherit'}} href="https://github.com/franciscoamaral0" rel="noopener noreferrer" target='_blank' ><GitHubIcon color = 'disable' fontSize = 'large'/></a>
           </div>
         </div>
       </footer>
