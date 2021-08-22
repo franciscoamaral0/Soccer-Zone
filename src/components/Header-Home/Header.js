82989a820421f62ecce25581f612f3bf46f283f8
Franciscoamaral0 <franciscogurgel@me.com>
2021-08-22 07:03:49 +0100

    repair header when collapse

--- src/components/Header-Home/Header.js
+++ src/components/Header-Home/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, Container } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 import ImageLogo from '../../images/03b0b151b42a829d87b3707368ec601d-removebg-preview.png'
 import Navbar from 'react-bootstrap/Navbar'
@@ -12,15 +12,13 @@ class Header extends Component {
   state = {  }
   render() { 
     return (  
-      <Navbar bg="light" expand="lg">
+      <Navbar collapseOnSelect bg="light" expand="lg">
+      
       <Navbar.Brand className='ps-5' href="#"> <Link to='/'> <img width="90px" src={ImageLogo} alt='Logo Soccer Zone'/></Link> </Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse className='ps-5' id="navbarScroll">
-      <Nav
-      className="mr-auto my-2 my-lg-0 "
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse className='ps-5' id="responsive-navbar-nav">
+      <Nav className="me-auto">
+
       <Nav.Link><NavLink to='/brasileirao' activeClassName="is-active" className="text-dark text-decoration-none test" >Campeonato Brasileiro </NavLink> </Nav.Link>
       <Nav.Link><NavLink to='/copa-do-brasil'  activeClassName="is-active" className="text-dark text-decoration-none test">Copa do Brasil</NavLink></Nav.Link>
       <Nav.Link><NavLink to='/libertadores'  activeClassName="is-active" className="text-dark text-decoration-none test">Libertadores</NavLink></Nav.Link>
@@ -28,6 +26,7 @@ class Header extends Component {
       <Nav.Link><NavLink to='/contato'  activeClassName="is-active" className="text-dark text-decoration-none test ">Contato</NavLink></Nav.Link>
     </Nav>
   </Navbar.Collapse>
+  
 </Navbar>
     );
   }
